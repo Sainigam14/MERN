e966a645d8aaefc25c612a75b0924c793eacadca
Sai Nigam Mandala <sainigammandala@gmail.com>
2023-10-05 15:16:05 +0530

    full stack app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react";
+import axios from "axios"
 
 function App() {
+  const[sname,setStudentName]=useState(" ");
+  const[tech,setTech]=useState(" ");
+  const[status,setStatus]=useState(" ");
+
+  const handleSubmit =()=>{
+    axios.post("http://localhost:9000/aliens",{name:sname,tech:tech,sub:status}).then(
+     console.log("success")
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+      <label>Student Name</label>
+      <input name="sname" value={sname} onChange={(e)=>{setStudentName(e.target.value)}} ></input>
+      </div>
+      <div>
+      <label>Technology</label>
+      <input name="tech" value={tech} onChange={(e)=>{setTech(e.target.value)}}></input>
+      </div>
+      <div>
+      <label>Status</label>
+      <input name="status" value={status} onChange={(e)=>{setStatus(e.target.value)}}></input>
+      </div>
+      <div>
+      <button onClick={handleSubmit()}>Submit</button>
+      </div>
+    
+  </div>
   );
 }
 
--- server/app.js
+++ server/app.js
@@ -2,7 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const alienRouter = require('./routes/aliens')
 const url = 'mongodb+srv://sainigammandala:Sainigam@cluster0.nykoehb.mongodb.net/?retryWrites=true&w=majority'
+const cors = require("cors")
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+
 mongoose.connect(url)
 const con = mongoose.connection
 con.on('open',() =>
